82998384afa0a77041151235f307d3322ca80942
anujs15 <anujsaraswat9027@gmail.com>
2025-07-12 01:36:28 +0530

    final

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -18,7 +18,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: ['http://localhost:5173',"https://url-shortner-snowy-eight.vercel.app"], 
     credentials: true 
 }));
 
